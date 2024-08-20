f1a0ea6880f3de28d3261a463bde1fe1481d8219
utkarshminds <pranav.nerurkar@utkarshminds.com>
2024-08-20 16:11:52 +0530

    final app

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # project_4_vacation_planner_openai
 Vacation planning using Open AI
+
+# step 1 - create virtual env
+# step 1a - create main.py
+# step 1b - create services/ , .streamlit/, secrets.toml, call_openai.py
+
+# step 2 - coding and open ai api key
+
+# step 3 - testing
---
+++ main.py
@@ -0,0 +1,57 @@
+import streamlit as st
+
+import hmac
+
+from services.call_openai import call_open_ai
+
+def check_password():
+    """Returns `True` if the user had a correct password."""
+
+    def login_form():
+        """Form with widgets to collect user information"""
+        with st.form("Credentials"):
+            st.text_input("Username", key="username")
+            st.text_input("Password", type="password", key="password")
+            st.form_submit_button("Log in", on_click=password_entered)
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if st.session_state["username"] in st.secrets[
+            "passwords"
+        ] and hmac.compare_digest(
+            st.session_state["password"],
+            st.secrets.passwords[st.session_state["username"]],
+        ):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the username or password.
+            del st.session_state["username"]
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the username + password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show inputs for username + password.
+    login_form()
+    if "password_correct" in st.session_state:
+        st.error("😕 User not known or password incorrect")
+    return False
+
+
+if not check_password():
+    st.stop()
+
+
+st.title('Three day Vacation planner')
+
+city_name = st.text_input('Enter the city name')
+st.write('City name =', city_name)
+
+num_days = st.text_input('Enter the number of vacation days')
+st.write('Number of days =', num_days)
+
+
+if st.button('Create a vacation plan'):
+    st.write('Button clicked')
+    call_open_ai(city_name, num_days)
